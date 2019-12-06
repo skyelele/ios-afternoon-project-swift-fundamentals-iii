fbdb03e212100650ded8aba499356750d273c0a6
Skye Leahy <user162867@74-80-250-207.ic2net.net>
2019-12-05 16:10:09 -0800

    first commit

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum currency {
+    case cad
+    case mxn
+}
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
@@ -19,21 +24,59 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cadButton: UIButton!
     @IBOutlet weak var mxnButton: UIButton!
+    
+    var currency : currency = .cad
 
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        
+        guard let userInput = Double(fromCurrencyTextField.text!) else {
+            return
+        }
+        fromCurrencyTextField.text = String(convert(userInput))
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+        if cadButton.isSelected == true {
+            cadButton.isSelected = false
+        } else {
+            cadButton.isSelected = true
+        }
         
+        if mxnButton.isSelected == true {
+            cadButton.isSelected = false
+            currency = .cad
+            toCurrencyLabel.text = "Currency (CAD)"
+        } else {
+            cadButton.isSelected = true
+        }
+
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+        if mxnButton.isSelected == true {
+            mxnButton.isSelected = false
+        } else {
+            mxnButton.isSelected = true
+        }
         
+        if cadButton.isSelected == true {
+            mxnButton.isSelected = false
+            currency = .cad
+            toCurrencyLabel.text = "Currency (MXN)"
+        } else {
+            mxnButton.isSelected = true
+        }
     }
     
     // MARK: - Helper Methods
+    func convert(_ dollars: Double) -> Double {
+        if currency == .cad {
+
+            return (dollars * 1.33)
+        } else {
+            return (dollars * 19.70)
+        }
+    }
 }
 
